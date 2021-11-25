8065aa49f06b0dd0cddfe9e3f9daf42c904bf112
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 14:23:08 -0500

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,4 @@
+twitter_credentials = dict(consumer_key='consumer_key',
+                           consumer_secret='consumer_secret',
+                           access_token_key='access_key',
+                           access_token_secret='access_secret')
