5acddef1ed8439a0fc71adcee0ca117edbbdb4c6
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 23:51:54 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -5,11 +5,11 @@
 
 
 #variables for accessing twitter API
-consumer_key='nODFKjiYzrF19xYefvQ6V1Npp'
-consumer_secret_key='OI0ubjR7i3G2VfFzqzeZuehuh7ytyuZeSHPar9zI8qXPXjfSzC'
-access_token='1463947072403652618-scdxO96qAWUFoGePy4ZLSoDmLRBeys'
-access_token_secret='LQipk1e8UWMIvhKgKPwhETEFIJ8UprzMfHppvPL8CrDqB'
-bearer_token='AAAAAAAAAAAAAAAAAAAAAHe8WAEAAAAAFOdG5KkJtdyPz05w%2FR2DYoJvs%2B4%3DpgO894Z6ltPgNyOqiiRJsjXgxTynzrybLmfegqzU8xJUGN0GkN'
+consumer_key='Dm9SMFE5ZTbt6WocJ5FKqMEF4'
+consumer_secret_key='GfO05LPZaatzwGs7OqEUku3ROQZPztQo2u2zQHfxkCRAkJmqML'
+access_token='1463947072403652618-RgLN9btVDl0TqGx70IzVYXrrgKSlq9'
+access_token_secret='2dWNl2iYRVHc2FBGJyPYl8IjnehXipe6wIjKCILbWHrXH'
+bearer_token='AAAAAAAAAAAAAAAAAAAAAHe8WAEAAAAA4eT8YnLcsl2yRzXvgRb9rLnSo%2Fg%3D8APTjm2PU9s6mmbFzf6cDkWUy9pcsYPiakrl8zVZmS70veBHF3'
 
 
 
