e4d3d190d2391937e1183c9e3d5308aa9435e8db
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 22:31:57 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -5,11 +5,11 @@
 
 
 #variables for accessing twitter API
-consumer_key=os.environ['nODFKjiYzrF19xYefvQ6V1Npp']
-consumer_secret_key=os.environ['OI0ubjR7i3G2VfFzqzeZuehuh7ytyuZeSHPar9zI8qXPXjfSzC']
-access_token=os.environ['1463947072403652618-scdxO96qAWUFoGePy4ZLSoDmLRBeys']
-access_token_secret=os.environ['LQipk1e8UWMIvhKgKPwhETEFIJ8UprzMfHppvPL8CrDqB']
-bearer_token=os.environ['AAAAAAAAAAAAAAAAAAAAAHe8WAEAAAAAFOdG5KkJtdyPz05w%2FR2DYoJvs%2B4%3DpgO894Z6ltPgNyOqiiRJsjXgxTynzrybLmfegqzU8xJUGN0GkN']
+consumer_key='nODFKjiYzrF19xYefvQ6V1Npp'
+consumer_secret_key='OI0ubjR7i3G2VfFzqzeZuehuh7ytyuZeSHPar9zI8qXPXjfSzC'
+access_token='1463947072403652618-scdxO96qAWUFoGePy4ZLSoDmLRBeys'
+access_token_secret='LQipk1e8UWMIvhKgKPwhETEFIJ8UprzMfHppvPL8CrDqB'
+bearer_token='AAAAAAAAAAAAAAAAAAAAAHe8WAEAAAAAFOdG5KkJtdyPz05w%2FR2DYoJvs%2B4%3DpgO894Z6ltPgNyOqiiRJsjXgxTynzrybLmfegqzU8xJUGN0GkN'
 
 
 
