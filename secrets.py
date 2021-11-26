4693338d517ccb45041a7f2747f23b11493a3f45
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 20:46:10 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
-twitter_credentials = dict(consumer_key= "btji7ufQvgGcINzwZZ9pV1rbS",
-                           consumer_secret= "Q91KQW5IBXfnWZF8Y0rPB1SaKXF0wA2dxi51poS2r4BCZWtHDB",
-                           access_token_key= "1463947072403652618-kk77vJEMYSuVCwBMdinH8GRUgnM3Pe",
-                           access_token_secret= "41YXimkgAwIse628WW2t2JWo2POdj5IFz6H7wVdgtkBqc")
+twitter_credentials = dict(consumer_key= 'btji7ufQvgGcINzwZZ9pV1rbS',
+                           consumer_secret= 'Q91KQW5IBXfnWZF8Y0rPB1SaKXF0wA2dxi51poS2r4BCZWtHDB',
+                           access_token_key= '1463947072403652618-kk77vJEMYSuVCwBMdinH8GRUgnM3Pe',
+                           access_token_secret= '41YXimkgAwIse628WW2t2JWo2POdj5IFz6H7wVdgtkBqc')
