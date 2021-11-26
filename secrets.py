00161ccac3e00f17c0014eb63ab231d71c268774
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 22:25:48 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -5,10 +5,7 @@
 
 
 #variables for accessing twitter API
-consumer_key=os.environ['consumer_key']
-consumer_secret_key=os.environ['consumer_secret_key']
-access_token=os.environ['access_token']
-access_token_secret=os.environ['access_token_secret']
-bearer_token=os.environ['bearer_token']
+
+consumer_key = {{ secret.CONSUMER_KEY }}
 
 
