639b351d64111f141207bdf0fb1847690f28d281
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 21:37:08 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
-twitter_credentials = dict(consumer_key= 'btji7ufQvgGcINzwZZ9pV1rbS',
-                          consumer_secret= 'Q91KQW5IBXfnWZF8Y0rPB1SaKXF0wA2dxi51poS2r4BCZWtHDB',
-                          access_token_key= '1463947072403652618-kk77vJEMYSuVCwBMdinH8GRUgnM3Pe',
-                          access_token_secret= '41YXimkgAwIse628WW2t2JWo2POdj5IFz6H7wVdgtkBqc')
+twitter_credentials = dict(consumer_key= 'nODFKjiYzrF19xYefvQ6V1Npp',
+                          consumer_secret= 'OI0ubjR7i3G2VfFzqzeZuehuh7ytyuZeSHPar9zI8qXPXjfSzC',
+                          access_token_key= '1463947072403652618-scdxO96qAWUFoGePy4ZLSoDmLRBeys',
+                          access_token_secret= 'LQipk1e8UWMIvhKgKPwhETEFIJ8UprzMfHppvPL8CrDqB')
 
 message=input('first tweet')
 api = twitter.Api(**twitter_credentials)
