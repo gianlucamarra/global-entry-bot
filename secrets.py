e2ec102d71767d623cd61ca93c4da14a34a1a1f1
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 21:37:46 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -2,6 +2,8 @@ twitter_credentials = dict(consumer_key= 'nODFKjiYzrF19xYefvQ6V1Npp',
                           consumer_secret= 'OI0ubjR7i3G2VfFzqzeZuehuh7ytyuZeSHPar9zI8qXPXjfSzC',
                           access_token_key= '1463947072403652618-scdxO96qAWUFoGePy4ZLSoDmLRBeys',
                           access_token_secret= 'LQipk1e8UWMIvhKgKPwhETEFIJ8UprzMfHppvPL8CrDqB')
+import numpy as np
+import twitter
 
 message=input('first tweet')
 api = twitter.Api(**twitter_credentials)
