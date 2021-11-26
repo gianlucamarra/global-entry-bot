a9a6c3bbdc2f3585ffafa44868a80c9eb84f4a36
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 20:57:41 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,19 +1,5 @@
-##twitter_credentials = dict(consumer_key= 'btji7ufQvgGcINzwZZ9pV1rbS',
-                           ##consumer_secret= 'Q91KQW5IBXfnWZF8Y0rPB1SaKXF0wA2dxi51poS2r4BCZWtHDB',
-                          ## access_token_key= '1463947072403652618-kk77vJEMYSuVCwBMdinH8GRUgnM3Pe',
-                          ## access_token_secret= '41YXimkgAwIse628WW2t2JWo2POdj5IFz6H7wVdgtkBqc')
-import numpy as np
-import tweepy
+twitter_credentials = dict(consumer_key= 'btji7ufQvgGcINzwZZ9pV1rbS',
+                          consumer_secret= 'Q91KQW5IBXfnWZF8Y0rPB1SaKXF0wA2dxi51poS2r4BCZWtHDB',
+                          access_token_key= '1463947072403652618-kk77vJEMYSuVCwBMdinH8GRUgnM3Pe',
+                          access_token_secret= '41YXimkgAwIse628WW2t2JWo2POdj5IFz6H7wVdgtkBqc')
 
-#variables for accessing twitter API
-consumer_key='C7NsZr5ywTTONwBzATUEqHo17'
-consumer_secret_key='2hD2QEzf2NwkpWF8c0iLEJLP4t9ox7UP6S6NzRKSS4p8MjI2yU'
-access_token='1463947072403652618-NJQsSHSfcpSGHe2dnh8HIfkdBx6Nrj'
-access_token_secret='poobrigDcP71WfgEW9x6WOflIYuvE41c3FoPb704aZUPj'
-      
-auth=tweepy.OAuthHandler(consumer_key,consumer_secret_key)
-auth.set_access_token(access_token,access_token_secret)
-api=tweepy.API(auth)
-tweet=input('one time')
-#Generate text tweet
-api.update_status(tweet)
