1467665cfa6694f68cdf32c6f52ecc7974a1621c
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 20:00:49 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
-twitter_credentials = dict(consumer_key='consumer_key',
-                           consumer_secret='consumer_secret',
-                           access_token_key='access_key',
-                           access_token_secret='access_secret')
+twitter_credentials = dict(consumer_key=${{ secrets.consumer_key}},
+                           consumer_secret=${{ secrets.consumer_secret}},
+                           access_token_key=${{ secrets.access_token_key}},
+                           access_token_secret=${{ secrets.access_token_secret}})
