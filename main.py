0fe912d13f81163ebbd6d92c96436d084512f67f
gianlucamarra <95047456+gianlucamarra@users.noreply.github.com>
2021-11-25 23:38:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ import twitter
 import tweepy
 
 
-consumer_key='nODFKjiYzrF19xYefvQ6V1Npp'
-consumer_secret_key='OI0ubjR7i3G2VfFzqzeZuehuh7ytyuZeSHPar9zI8qXPXjfSzC'
-access_token='1463947072403652618-scdxO96qAWUFoGePy4ZLSoDmLRBeys'
-access_token_secret='LQipk1e8UWMIvhKgKPwhETEFIJ8UprzMfHppvPL8CrDqB'
-bearer_token='AAAAAAAAAAAAAAAAAAAAAHe8WAEAAAAAFOdG5KkJtdyPz05w%2FR2DYoJvs%2B4%3DpgO894Z6ltPgNyOqiiRJsjXgxTynzrybLmfegqzU8xJUGN0GkN'
 
 
 tweeter_obj = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret_key,
